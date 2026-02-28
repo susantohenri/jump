78c67d502010bf02c67fca9f1d6b981b974693c6
Henri Susanto <henry.dinus@gmail.com>
2026-02-28 11:39:47 +0700

    fix: footer container

--- application/views/index.php
+++ application/views/index.php
@@ -86,12 +86,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      henry.dinus@gmail.com 081901088918
+    <div class="container">
+      <!-- To the right -->
+      <div class="float-right d-none d-sm-inline">
+        henry.dinus@gmail.com 081901088918
+      </div>
+      <!-- Default to the left -->
+      <small><strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.</small>
     </div>
-    <!-- Default to the left -->
-    <small><strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.</small>
   </footer>
 </div>
 <!-- ./wrapper -->
